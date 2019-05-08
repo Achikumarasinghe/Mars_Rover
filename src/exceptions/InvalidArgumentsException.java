c25e169e4f0cac1345e89a677dfaad9cf99333ba
Achikumarasinghe <iamtheqou@gmail.com>
2019-05-08 07:27:51 +0530

    eddited commets 1

--- src/exceptions/InvalidArgumentsException.java
+++ src/exceptions/InvalidArgumentsException.java
@@ -1,14 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * @author Achini Kumarasinghe
+ * Date:07-05-2019
+ * Created using NetBeans IDE 8.2
  */
 package exceptions;
 
-/**
- *
- * @author USER
- */
 public class InvalidArgumentsException extends Exception{
     public InvalidArgumentsException(String msg){
         super(msg);
